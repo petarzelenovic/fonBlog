1f283dc1279faeded5517575b5c8c9c3e161a86e
petarzelenovic <petarzelenovic261@gmail.com>
2024-05-16 01:43:14 +0200

    Signup route

--- api/index.js
+++ api/index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 dotenv.config();
 
+//Konektovanje na bazu
 mongoose
     .connect(process.env.MONGO)
     .then(() => {
@@ -12,7 +14,16 @@ mongoose
     .catch((err) => {
         console.log(err);
     });
+
+//pravljenje express apl
 const app = express();
+app.use(express.json()); //dozvoljavamo da mozemo slati json serveru
+
+//Pokretanje servera
 app.listen(3000, () => {
     console.log("Server is running!");
 });
+
+//Pravljenje middleware fja za rute
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
