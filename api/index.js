62f5ba5c348d542dab6c438630576d445c8e7e34
petarzelenovic <petarzelenovic261@gmail.com>
2024-05-16 01:01:31 +0200

    Connection to db

--- api/index.js
+++ api/index.js
@@ -1,5 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
+
+mongoose
+    .connect(
+        "mongodb+srv://petar:petar@fon-blog.xbn8otm.mongodb.net/fon-blog?retryWrites=true&w=majority&appName=fon-blog"
+    )
+    .then(() => {
+        console.log("MongoDB is connected");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 const app = express();
 app.listen(3000, () => {
     console.log("Server is running!");
