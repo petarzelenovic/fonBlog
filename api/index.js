8a88fb7db2e9814d58acac9afc61cd017cc563bc
petarzelenovic <petarzelenovic261@gmail.com>
2024-05-16 02:00:13 +0200

    Error handler and middleware

--- api/index.js
+++ api/index.js
@@ -27,3 +27,11 @@ app.listen(3000, () => {
 //Pravljenje middleware fja za rute
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+
+//Middleware kojim cemo obratiti gresku ako dodje od gore
+app.use((err, req, res, next) => {
+    //next za prelazak na sledeci middleware
+    const statusCode = err.statusCode || 500; //ako ne postavimo statusCode dobijamo gresku
+    const message = err.message || "Internal Server Error";
+    res.status(statusCode).json({ success: false, statusCode, message });
+});
