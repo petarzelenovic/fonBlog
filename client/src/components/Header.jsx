c870734643d97ce00d82faeecf5368f4c6ea1566
petarzelenovic <petarzelenovic261@gmail.com>
2024-05-16 21:16:03 +0200

    sign in header updating

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import { Button, Navbar, TextInput } from "flowbite-react";
+import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react";
 import { Link, useLocation } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon } from "react-icons/fa";
 import Logo from "../images/fon-logo2.png";
+import { useSelector } from "react-redux";
 
 export default function Header() {
     const path = useLocation().pathname;
+    const { currentUser } = useSelector((state) => state.user);
     return (
         <Navbar className="border-b-2">
             <Link
@@ -27,13 +29,48 @@ export default function Header() {
                 <AiOutlineSearch />
             </Button>
             <div className="flex gap-2 md:order-2">
-                <Link to="/sign-in">
-                    <Button outline pill gradientDuoTone="cyanToBlue">
-                        Sign In
-                    </Button>
-                </Link>
+                {currentUser ? (
+                    <Dropdown
+                        arrowIcon={false}
+                        inline
+                        label={<Avatar alt="user" rounded></Avatar>}
+                    >
+                        <Dropdown.Header>
+                            <div>
+                                <span className="font-semibold mr-1">
+                                    {" "}
+                                    Username:
+                                </span>
+                                <span className=" text-sm ">
+                                    {currentUser.username}
+                                </span>
+                            </div>
+                            <div className="mt-1">
+                                <span className="font-semibold mr-1">
+                                    Email:
+                                </span>
+                                <span className="text-sm  truncate">
+                                    {currentUser.email}
+                                </span>
+                            </div>
+                        </Dropdown.Header>
+                        <Link to="/dashboard?tab=profile">
+                            <Dropdown.Item>Profile</Dropdown.Item>
+                        </Link>
+                        <Dropdown.Divider></Dropdown.Divider>
+                        <Dropdown.Item>Sign out</Dropdown.Item>
+                    </Dropdown>
+                ) : (
+                    <Link to="/sign-in">
+                        <Button outline pill gradientDuoTone="cyanToBlue">
+                            Sign In
+                        </Button>
+                    </Link>
+                )}
+
                 {/* <Navbar.Toggle /> */}
             </div>
+
             {/* <Navbar.Collapse>
                 <Navbar.Link active={path === "/"} as={"div"}>
                     <Link to="/">Home</Link>
