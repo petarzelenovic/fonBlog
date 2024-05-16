d979e04fb34d2ca8fcc30d82048226ce8130c217
petarzelenovic <petarzelenovic261@gmail.com>
2024-05-16 02:30:51 +0200

    Sign up page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,5 +1,63 @@
 import React from "react";
+import Logo from "../assets/fon-logo.png";
+import { Link } from "react-router-dom";
+import { Button, Label, TextInput } from "flowbite-react";
 
 export default function SignUp() {
-    return <div>SignUp</div>;
+    return (
+        <div className="min-h-screen mt-20">
+            <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
+                <div className="flex-1">
+                    <Link
+                        to="/"
+                        className="self-center  text-sm sm:text-xl font-bold"
+                    >
+                        <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white text-4xl">
+                            FonBlog
+                        </span>
+                    </Link>
+                    <p className="text-sm mt-5">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    </p>
+                </div>
+                <div className="flex-1">
+                    <form className="flex flex-col gap-4">
+                        <div>
+                            <Label value="Your username" />
+                            <TextInput
+                                type="text"
+                                placeholder="Username"
+                                id="username"
+                            />
+                        </div>
+                        <div>
+                            <Label value="Your email" />
+                            <TextInput
+                                type="text"
+                                placeholder="name@fon.bg.ac.rs"
+                                id="email"
+                            />
+                        </div>
+                        <div>
+                            <Label value="Your password" />
+                            <TextInput
+                                type="password"
+                                placeholder="Password"
+                                id="password"
+                            />
+                        </div>
+                        <Button gradientDuoTone="purpleToPink" type="submit">
+                            Sign up
+                        </Button>
+                    </form>
+                    <div className="flex gap-2 text-sm mt-5">
+                        <span>Have an account?</span>
+                        <Link to="/sign-in" className="text-blue-500">
+                            Sign in
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
