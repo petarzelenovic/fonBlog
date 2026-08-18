ccba9b95dfd408246032675dbdb15866dd63a70a
Petar Zelenovic <petarzelenovic261@gmail.com>
2026-08-18 12:42:13 +0200

    creating auth route

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import useRoutes from "./routes/user.route.js";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.routes.js";
 
 dotenv.config();
 
@@ -16,8 +17,11 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
 
-app.use("/api/user", useRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
