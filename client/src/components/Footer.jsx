91e6d25e0502d2f89e6af557a1759be7ccf35f8e
Petar Zelenovic <petarzelenovic261@gmail.com>
2026-08-18 14:31:10 +0200

    creating footer component

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import {
+  Footer,
+  FooterLink,
+  FooterLinkGroup,
+  FooterTitle,
+} from "flowbite-react";
+import { Link } from "react-router-dom";
+
+export default function FooterComponent() {
+  return (
+    <Footer container className="border border-t-8 border-teal-500">
+      <div className="w-full max-w-7xl mx-auto">
+        <div className="grid w-full justify-between sm:flex md:grid-cols-1">
+          <div>
+            <Link
+              to="/"
+              className="self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white"
+            >
+              <span className="px-2 py-1 bg-linear-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+                Fon
+              </span>
+              Blog
+            </Link>
+          </div>
+          <div className="grid grid-cols-2 gap-3 sm: mt-5 sm:grid-cols-3 sm:gap-6">
+            <div>
+              {" "}
+              <FooterTitle title="About" />
+              <FooterLinkGroup col>
+                <FooterLink href="/" target="_blank" rel="noopener noreferrer">
+                  Some link
+                </FooterLink>
+                <FooterLink href="/" target="_blank" rel="noopener noreferrer">
+                  Fon blog
+                </FooterLink>
+              </FooterLinkGroup>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Footer>
+  );
+}
