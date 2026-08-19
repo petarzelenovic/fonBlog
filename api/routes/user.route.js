3ddf8d4117213299123645c4b4c7665f85af7201
Petar Zelenovic <petarzelenovic261@gmail.com>
2026-08-19 11:53:36 +0200

    delete api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { updateUser } from "../controllers/user.controller.js";
+import { updateUser, deleteUser } from "../controllers/user.controller.js";
 import { verifyUser } from "../utils/verifyUser.js";
 const router = express.Router();
 
 router.put("/update/:userId", verifyUser, updateUser);
+router.delete("/delete/:userId", verifyUser, deleteUser);
 
 export default router;
