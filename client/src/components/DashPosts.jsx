8a1233f5decd269aeed8d53ca707f1e365738fe7
Petar Zelenovic <petarzelenovic261@gmail.com>
2026-08-19 16:23:31 +0200

    dashboard pagination

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -3,6 +3,7 @@ import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useState } from "react";
 import {
+  Pagination,
   Table,
   TableBody,
   TableCell,
@@ -16,15 +17,22 @@ export default function DashPosts() {
   const { currentUser } = useSelector((state) => state.user);
 
   const [userPosts, setUserPosts] = useState([]);
+  const [totalPages, setTotalPages] = useState(1);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPosts, setTotalPosts] = useState(0);
+
   useEffect(() => {
     const fetchPosts = async () => {
       try {
+        const startIndex = (currentPage - 1) * 9;
         const response = await fetch(
-          `/api/post/getposts?userId=${currentUser._id}`,
+          `/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`,
         );
         const data = await response.json();
         if (response.ok) {
           setUserPosts(data.posts);
+          setTotalPosts(data.totalPosts);
+          setTotalPages(Math.ceil(data.totalPosts / 9) || 1);
         }
       } catch (error) {
         console.log(error.message);
@@ -33,7 +41,7 @@ export default function DashPosts() {
     if (currentUser.isAdmin) {
       fetchPosts();
     }
-  }, [currentUser._id]);
+  }, [currentUser._id, currentPage]);
 
   return (
     <div className="table-auto overflow-x-scroll md:mx-auto p-3 w-full">
@@ -53,54 +61,56 @@ export default function DashPosts() {
               </TableRow>
             </TableHead>
             <TableBody className="divide-y">
-              {userPosts.map(
-                (post) => (
-                  console.log(post),
-                  (
-                    <TableRow
-                      key={post._id}
-                      className="bg-white dark:border-gray-700 dark:bg-gray-800"
+              {userPosts.map((post) => (
+                <TableRow
+                  key={post._id}
+                  className="bg-white dark:border-gray-700 dark:bg-gray-800"
+                >
+                  <TableCell>
+                    {new Date(post.updatedAt).toLocaleDateString()}
+                  </TableCell>
+                  <TableCell>
+                    <Link to={`/post/${post.slug}`}>
+                      <img
+                        src={post.image}
+                        alt={post.title}
+                        className="h-10 w-20 object-cover bg-gray-500"
+                      />
+                    </Link>
+                  </TableCell>
+                  <TableCell>
+                    <Link
+                      className="font-medium text-gray-900 dark:text-white"
+                      to={`/post/${post.slug}`}
                     >
-                      <TableCell>
-                        {new Date(post.updatedAt).toLocaleDateString()}
-                      </TableCell>
-                      <TableCell>
-                        <Link to={`/post/${post.slug}`}>
-                          <img
-                            src={post.image}
-                            alt={post.title}
-                            className="h-10 w-20 object-cover bg-gray-500"
-                          />
-                        </Link>
-                      </TableCell>
-                      <TableCell>
-                        <Link
-                          className="font-medium text-gray-900 dark:text-white"
-                          to={`/post/${post.slug}`}
-                        >
-                          {post.title}
-                        </Link>
-                      </TableCell>
-                      <TableCell>{post.category}</TableCell>
-                      <TableCell>
-                        <span className="cursor-pointer font-medium text-red-500 hover:underline">
-                          Delete
-                        </span>
-                      </TableCell>
-                      <TableCell>
-                        <Link
-                          className="text-teal-500 hover:underline"
-                          to={`/update-post/${post._id}`}
-                        >
-                          <span>Edit</span>
-                        </Link>
-                      </TableCell>
-                    </TableRow>
-                  )
-                ),
-              )}
+                      {post.title}
+                    </Link>
+                  </TableCell>
+                  <TableCell>{post.category}</TableCell>
+                  <TableCell>
+                    <span className="cursor-pointer font-medium text-red-500 hover:underline">
+                      Delete
+                    </span>
+                  </TableCell>
+                  <TableCell>
+                    <Link
+                      className="text-teal-500 hover:underline"
+                      to={`/update-post/${post._id}`}
+                    >
+                      <span>Edit</span>
+                    </Link>
+                  </TableCell>
+                </TableRow>
+              ))}
             </TableBody>
           </Table>
+          <Pagination
+            currentPage={Number(currentPage)}
+            totalPages={totalPages}
+            onPageChange={(page) => setCurrentPage(page)}
+            showIcons
+            className="mt-4"
+          />
         </>
       ) : (
         <div>
