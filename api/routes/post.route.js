3057f14b2eb63c8f966f2e3e1985ba7ad81d1d82
Petar Zelenovic <petarzelenovic261@gmail.com>
2026-08-19 15:55:01 +0200

    getposts api route

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,7 +1,8 @@
 import express from "express";
 import { verifyUser } from "../utils/verifyUser.js";
-import { create } from "../controllers/post.controller.js";
+import { create, getPosts } from "../controllers/post.controller.js";
 const router = express.Router();
 
 router.post("/create", verifyUser, create);
+router.get("/getposts", getPosts);
 export default router;
