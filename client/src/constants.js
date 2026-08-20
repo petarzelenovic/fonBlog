84e7703659d4ab274504c34608d4f540388776b4
Petar Zelenovic <petarzelenovic261@gmail.com>
2026-08-20 21:57:31 +0200

    firebase image size upload

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,5 +1,6 @@
 export const POSTS_LIMIT = 9;
 export const SHORT_DESCRIPTION_LIMIT = 200;
+export const MAX_IMAGE_SIZE = 3 * 1024 * 1024;
 export const TOAST_DURATION = 2500;
 export const USERNAME_PATTERN = /^[a-zA-Z0-9._]+$/;
 export const DEFAULT_CATEGORY_COLOR = "#004A7C";
--- client/src/utils/uploadImage.js
+++ client/src/utils/uploadImage.js
@@ -4,6 +4,7 @@ import {
   ref,
   uploadBytesResumable,
 } from "firebase/storage";
+import { MAX_IMAGE_SIZE } from "../constants";
 import { app } from "../firebase";
 
 export function uploadImage(file, onProgress) {
@@ -12,11 +13,21 @@ export function uploadImage(file, onProgress) {
       reject(new Error("Nema fajla za otpremanje"));
       return;
     }
+    if (!file.type.startsWith("image/")) {
+      reject(new Error("Možeš otpremiti samo slike"));
+      return;
+    }
+    if (file.size >= MAX_IMAGE_SIZE) {
+      reject(new Error("Slika mora biti manja od 3 MB"));
+      return;
+    }
 
     const storage = getStorage(app);
     const fileName = `${Date.now()}-${file.name}`;
     const storageRef = ref(storage, fileName);
-    const uploadTask = uploadBytesResumable(storageRef, file);
+    const uploadTask = uploadBytesResumable(storageRef, file, {
+      contentType: file.type,
+    });
 
     uploadTask.on(
       "state_changed",
